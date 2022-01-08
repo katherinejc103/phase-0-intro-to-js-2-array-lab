e25a009cd1487f5005b698f6d7623cfb54c39c71
Katherine Jean-Charles <katherinejc103@gmail.com>
2022-01-08 16:47:00 -0500

    Done

--- index.js
+++ index.js
@@ -1,5 +1,34 @@
 // Write your solution here!
 const cats = ["Milo", "Otis", "Garfield"];
 
-cats.push("Ralph");
-cats;
+function destructivelyAppendCat(name) {
+    return cats.push(name);  
+} 
+
+function destructivelyPrependCat(name) {
+    return cats.unshift(name);
+}
+
+function destructivelyRemoveLastCat() {
+    return cats.pop();
+}
+
+function destructivelyRemoveFirstCat() {
+    return cats.shift();
+}
+
+function appendCat(name) {
+    return [...cats, name];
+}
+
+function prependCat(name) {
+    return [name, ...cats]
+}
+
+function removeLastCat() {
+    return cats.slice(0,2);
+}
+
+function removeFirstCat() {
+    return [cats[1], cats[2]]
+}
